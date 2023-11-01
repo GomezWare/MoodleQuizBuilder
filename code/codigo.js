341d1004bd7c4b5771c416a2fa44106e05acc83a
StreetNinjaCoder <streetninjacoder@gmail.com>
2023-11-01 03:10:20 +0100

    Plnaificacion inicial

--- code/codigo.js
+++ code/codigo.js
@@ -1,11 +1,22 @@
 "use strict";
 //FUNCTION
 function addPregunta() {
-  let nombre = String(document.querySelector("#nombre").value).trim;
-  let rCorrecta = String(document.querySelector("#rCorrecta").value).trim;
-  let rIncorrecta1 = String(document.querySelector("#rIncorrecta1").value).trim;
-  let rIncorrecta2 = String(document.querySelector("#rIncorrecta2").value).trim;
-  let rIncorrecta3 = String(document.querySelector("#rIncorrecta3").value).trim;
+
+  //TODO aqui habria que escapear los caracteres especiales (regex replace??)
+
+  //Se recuperan los datos y se parsean
+  //HACK se deberian hacer clases para poder gestionar mejor el codigo no vendria mal refactorizar
+  let nombre = String(document.querySelector("#nombre").value).trim();
+  let rCorrecta = String(document.querySelector("#rCorrecta").value).trim();
+  let rIncorrecta1 = String(
+    document.querySelector("#rIncorrecta1").value
+  ).trim();
+  let rIncorrecta2 = String(
+    document.querySelector("#rIncorrecta2").value
+  ).trim();
+  let rIncorrecta3 = String(
+    document.querySelector("#rIncorrecta3").value
+  ).trim();
 
   let divErrores = document.querySelector("#divErrores");
   let divPreguntas = document.querySelector("#divPreguntas");
@@ -14,14 +25,29 @@ function addPregunta() {
     rCorrecta.length == 0 ||
     rIncorrecta1.length == 0 ||
     rIncorrecta2.length == 0 ||
-    rIncorrecta3 == 0
+    rIncorrecta3.length == 0
   ) {
-    divErrores.innerHTML='<h1 id="error">Rellene todos los campos</h1>';
+    divPreguntas.innerHTML = "";
+    divErrores.innerHTML = '<h1 id="error">Rellene todos los campos</h1>';
   } else {
-    divPreguntas.innerHTML='<h1>Esta bien</h1>';
+    //TODO Crear el objeto prgunta, añadirlo al array y hacer que el propio objeto devuleva el div que inserta este metodo
+    divErrores.innerHTML = "";
+    divPreguntas.innerHTML = "<h1>Esta bien</h1>";
   }
+
+  //TODO Lo ideal seria una funcion que que imprimera el array de preguntas a modo de bucle for y se pudiera llamar desde todos los metodos que repinten la pantalla.
 }
 
+//TODO Funcion generar archivo
+
+//TODO Funcion Borra Preguntas
+
+//FIXME Crear pregunta vs Guardar pregunta???
+
+//TODO Diseñar un metodo para que se pueda descartar la pregunta o recuperarla
+
 //MAIN
-let btnPregunta = document.querySelector('#addPregunta');
-btnPregunta.addEventListener('click', addPregunta);
+
+// EVENTS LISTENERS
+let btnPregunta = document.querySelector("#addPregunta");
+btnPregunta.addEventListener("click", addPregunta);
--- code/objetos.js
+++ code/objetos.js
@@ -1,10 +1,15 @@
+"use strict";
 //OBJETOS
 
 let cuestionario = {
   preguntas: [],
-  addPregunta(Pregunta){
+  addPregunta(Pregunta) {
     this.preguntas.push(Pregunta);
-  }
+  },
+
+  /*TODO Buscar la manera de meter este objeto en el localstorage, quizas se pueda con un JSON Stringify y 
+  luego recuperar el propio objeto y sub-ojetos(array) con un JSON to Object???
+   */
 };
 
 function Pregunta(nombre, rCorrecta, rIncorrecta1, rIncorrecta2, rIncorrecta3) {
@@ -14,6 +19,6 @@ function Pregunta(nombre, rCorrecta, rIncorrecta1, rIncorrecta2, rIncorrecta3) {
   this.rIncorrecta2 = rIncorrecta2;
   this.rIncorrecta3 = rIncorrecta3;
 
-  //Esto tiene que devolcer un DIV con los datos de arriba
+  //TODO Esto tiene que devolver un DIV con los datos de arriba
   this.preguntaToHTMLDiv = function () {};
 }
